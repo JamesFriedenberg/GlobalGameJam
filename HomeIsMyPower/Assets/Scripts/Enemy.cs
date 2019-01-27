e7b0b135dee8683e4a447f866088ef5f353ba9e4
Herman McElveen (RIT Student) <hxm1417@ad.rit.edu>
2019-01-27 12:52:21 -0500

    nav mesh enemies

---
+++ HomeIsMyPower/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public NavMeshAgent agent;
+    public GameObject Player;
+
+	// Use this for initialization
+	private void Start()
+	{
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		if(Vector3.Distance(transform.position, Player.transform.position) < 20) {
+			agent.destination = Player.transform.position;
+		}
+	}
+}
