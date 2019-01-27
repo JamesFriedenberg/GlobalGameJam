708153d32e04e8d4f9f48d49e38cd0563cdbc412
Herman McElveen (RIT Student) <hxm1417@ad.rit.edu>
2019-01-27 11:44:52 -0500

    bhsnjd

---
+++ HomeIsMyPower/Assets/Scripts/FireBeam.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBeam : MonoBehaviour {
+
+    public GameObject bulletObj;
+    public List<GameObject> bulletList;
+    public List<float> destroyTimes;
+    public float bulletDestroyDelay = 2.0f;
+    public int bulletSpeed;
+    private float currentTime;
+    
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // set current time
+        currentTime = Time.time;
+        // check if the use presses the f key, then fire bullet
+        if (Input.GetKeyDown(KeyCode.F)) {
+            CreateBullet();
+            bulletList[bulletList.Count - 1].transform.forward = GameObject.FindGameObjectWithTag("Player").transform.forward;
+        }
+        // check if there are any bullets in the list
+        if (bulletList.Count > 0) {
+            for (int i = 0; i < bulletList.Count; i++) {
+                MoveBullet(i);
+                
+            }
+        }
+
+        DestroyBullets();
+	}
+
+    void CreateBullet() {
+
+        bulletList.Add(Object.Instantiate(bulletObj, gameObject.transform.GetChild(1).gameObject.transform.position, Quaternion.identity));
+        // bulletList[bulletList.Count].transform.forward = GameObject.FindGameObjectWithTag("Player").transform.forward;
+        destroyTimes.Add(Time.time);
+    }
+
+    void MoveBullet(int bulletToMove) {
+
+        // bulletList[bulletToMove].transform.forward = GameObject.FindGameObjectWithTag("Player").transform.forward;
+        bulletList[bulletToMove].transform.Translate(Vector3.forward * (Time.deltaTime * bulletSpeed));
+        
+    }
+
+    void DestroyBullets() {
+
+        for (int i = 0; i < bulletList.Count; i++) {
+            if (currentTime - destroyTimes[i] >= bulletDestroyDelay){
+                Destroy(bulletList[i]);
+                bulletList.Remove(bulletList[i]);
+
+                destroyTimes.Remove(destroyTimes[i]);
+            }
+        }
+        
+    }
+}
---
+++ HomeIsMyPower/Assets/Scripts/UIStat.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIStat : MonoBehaviour {
+
+    // UI Stuff
+    private Image content;
+    public float currFill;
+    public float currVal;
+    public float lerpSpeed;
+
+    public float maxVal { get; set; }
+
+    public float CurrVal
+    {
+        get
+        {
+            return currVal;
+        }
+
+        set
+        {
+            if (value > maxVal)
+            {
+                currVal = maxVal;
+            }
+            else if (value < 0)
+            {
+                currVal = 0;
+            }
+            else
+            {
+                currVal = value;
+            }
+
+            currFill = currVal / maxVal;
+        }
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+        content = GetComponent<Image>();
+        maxVal = 100;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (currFill != content.fillAmount)
+        {
+            content.fillAmount = Mathf.Lerp(content.fillAmount, currFill, Time.deltaTime * lerpSpeed);
+        }
+    }
+
+    public void Init(float currValue, float maxValue)
+    {
+        maxVal = maxValue;
+        currVal = currValue;
+        currFill = CurrVal;
+    }
+}
